9a70b47bd537a54cda6f0847da0a1229b27b91d1
Mauricio Robayo <rfmajo@gmail.com>
2020-01-22 20:39:50 +0000

    refactor: move vars declaration outside test funcs

--- __tests__/authorization.js
+++ __tests__/authorization.js
@@ -9,19 +9,20 @@ jest.mock('../src/authorization/helpers', () => ({
   randomString: jest.fn(() => 'kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg'),
 }))
 
+const requestMethod = 'post'
+const baseUrl = 'https://api.twitter.com/1.1/statuses/update.json'
+const queryParams = { include_entities: true }
+const bodyParams = {
+  status: 'Hello Ladies + Gentlemen, a signed OAuth request!',
+}
+const oauthOptions = {
+  api_key: 'xvz1evFS4wEEPTGEFPHBog',
+  api_secret_key: 'kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw',
+  access_token: '370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb',
+  access_token_secret: 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE',
+}
+
 it('should return the authorization header string', () => {
-  const requestMethod = 'post'
-  const baseUrl = 'https://api.twitter.com/1.1/statuses/update.json'
-  const queryParams = { include_entities: true }
-  const bodyParams = {
-    status: 'Hello Ladies + Gentlemen, a signed OAuth request!',
-  }
-  const oauthOptions = {
-    api_key: 'xvz1evFS4wEEPTGEFPHBog',
-    api_secret_key: 'kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw',
-    access_token: '370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb',
-    access_token_secret: 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE',
-  }
   expect(
     authorization({
       requestMethod,
--- __tests__/modules/signature.js
+++ __tests__/modules/signature.js
@@ -1,20 +1,21 @@
 const { signature } = require('../../src/authorization/modules/signature')
 
+const requestMethod = 'post'
+const baseUrl = 'https://api.twitter.com/1.1/statuses/update.json'
+const queryParams = { include_entities: true }
+const bodyParams = {
+  status: 'Hello Ladies + Gentlemen, a signed OAuth request!',
+}
+const oauthOptions = {
+  api_key: 'xvz1evFS4wEEPTGEFPHBog',
+  access_token: '370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb',
+  oauth_nonce: 'kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg',
+  oauth_timestamp: 1318622958,
+  api_secret_key: 'kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw',
+  access_token_secret: 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE',
+}
+
 it('should return the signature', () => {
-  const requestMethod = 'post'
-  const baseUrl = 'https://api.twitter.com/1.1/statuses/update.json'
-  const queryParams = { include_entities: true }
-  const bodyParams = {
-    status: 'Hello Ladies + Gentlemen, a signed OAuth request!',
-  }
-  const oauthOptions = {
-    api_key: 'xvz1evFS4wEEPTGEFPHBog',
-    access_token: '370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb',
-    oauth_nonce: 'kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg',
-    oauth_timestamp: 1318622958,
-    api_secret_key: 'kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw',
-    access_token_secret: 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE',
-  }
   expect(
     signature({
       requestMethod,
