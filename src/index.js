3f3aa350b1e46657f2a3dcfd3183cf0957964ea4
Mauricio Robayo <rfmajo@gmail.com>
2020-01-22 18:22:59 +0000

    refactor: split main function

--- src/index.js
+++ src/index.js
@@ -4,41 +4,23 @@ const { URL, URLSearchParams } = require('url')
 const { percentEncode } = require('./authorization/helpers')
 const { authorization } = require('./authorization/authorization')
 
-module.exports = options => {
-  const {
-    subdomain = 'api',
-    endpoint,
-    requestMethod = 'GET',
-    queryParams = {},
-    bodyParams = {},
-    oauthOptions,
-  } = options
-  const baseUrl = `https://${subdomain}.twitter.com/1.1${endpoint}`
-  const body = qs.stringify(bodyParams, '&', '=', {
-    encodeURIComponent: percentEncode,
-  })
+function buildUrl(baseUrl, queryParams) {
   const url = new URL(baseUrl)
   url.search = new URLSearchParams(queryParams)
-  const httpsOptions = {
-    protocol: url.protocol,
-    host: url.host,
-    hostname: url.hostname,
-    method: requestMethod,
-    path: `${url.pathname}${url.search}`,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(body),
-      Authorization: authorization(
-        requestMethod,
-        baseUrl,
-        queryParams,
-        bodyParams,
-        oauthOptions
-      ),
-      'cache-control': 'no-cache',
-    },
-  }
+  return url
+}
 
+function buildBody(bodyParams) {
+  return qs.stringify(bodyParams, '&', '=', {
+    encodeURIComponent: percentEncode,
+  })
+}
+
+function buildBaseUrl(subdomain, endpoint) {
+  return `https://${subdomain}.twitter.com/1.1${endpoint}`
+}
+
+function request(httpsOptions, body) {
   return new Promise((resolve, reject) => {
     const req = https.request(httpsOptions, res => {
       let data = ''
@@ -55,4 +37,39 @@ module.exports = options => {
     req.write(body)
     req.end()
   })
+}
+
+module.exports = options => {
+  const {
+    subdomain = 'api',
+    endpoint,
+    requestMethod = 'GET',
+    queryParams = {},
+    bodyParams = {},
+    oauthOptions,
+  } = options
+  const baseUrl = buildBaseUrl(subdomain, endpoint)
+  const body = buildBody(bodyParams)
+  const url = buildUrl(baseUrl, queryParams)
+  const httpsOptions = {
+    protocol: url.protocol,
+    host: url.host,
+    hostname: url.hostname,
+    method: requestMethod,
+    path: `${url.pathname}${url.search}`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(body),
+      Authorization: authorization(
+        requestMethod,
+        baseUrl,
+        queryParams,
+        bodyParams,
+        oauthOptions,
+      ),
+      'cache-control': 'no-cache',
+    },
+  }
+
+  return request(httpsOptions, body)
 }
