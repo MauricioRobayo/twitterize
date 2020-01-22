c48ea651b973fcecc3521034571df957c3ff95fe
Mauricio Robayo <rfmajo@gmail.com>
2020-01-22 18:29:47 +0000

    refactor(signature): use object literal as argument

--- src/authorization/modules/signature.js
+++ src/authorization/modules/signature.js
@@ -2,19 +2,19 @@ const crypto = require('crypto')
 const { signatureBaseString } = require('./signatureBaseString')
 const { percentEncode } = require('../helpers')
 
-exports.signature = (
+exports.signature = ({
   requestMethod,
   baseUrl,
   queryParams,
   bodyParams,
-  oauthOptions
-) => {
+  oauthOptions,
+}) => {
   const baseString = signatureBaseString(
     requestMethod,
     baseUrl,
     queryParams,
     bodyParams,
-    oauthOptions
+    oauthOptions,
   )
   const consumerSecret = percentEncode(oauthOptions.api_secret_key)
   const tokenSecret = percentEncode(oauthOptions.access_token_secret)
