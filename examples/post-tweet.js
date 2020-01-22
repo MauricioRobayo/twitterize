9cee905e770d1758d09934fe11dab72182d0d13b
Mauricio Robayo <rfmajo@gmail.com>
2020-01-22 21:09:25 +0000

    chore: update examples

--- examples/post-tweet.js
+++ examples/post-tweet.js
@@ -4,20 +4,19 @@ const twitterize = require('../src')
 // https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update.html
 
 // https://developer.twitter.com/en/apps/
-const oauthOptions = {
+const twit = twitterize({
   api_key: process.env.TWITTER_API_KEY,
   api_secret_key: process.env.TWITTER_API_SECRET_KEY,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-}
+})
 
 const options = {
   requestMethod: 'POST',
   endpoint: '/statuses/update.json',
   bodyParams: { status: 'Hello World!' },
-  oauthOptions,
 }
 
-twitterize(options)
+twit(options)
   .then(console.log)
   .catch(console.log)
