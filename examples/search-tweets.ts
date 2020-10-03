88260eed41d8f1dc7efbeb2f95584981bd2aebaf
Mauricio Robayo <rfmajo@gmail.com>
2020-10-02 19:14:38 -0500

    feat: add type definitions
    closes #43

---
+++ examples/search-tweets.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+// https://developer.twitter.com/en/apps/
+import twitterize, { OAuthOptions, RequestOptions } from '../src'
+
+interface Tweets {
+  statuses: []
+  search_metadata: Record<string, string | number>
+}
+
+const oAuthOptions: OAuthOptions = {
+  api_key: process.env.TWITTER_API_KEY || '',
+  api_secret_key: process.env.TWITTER_API_SECRET_KEY || '',
+  access_token: process.env.TWITTER_ACCESS_TOKEN || '',
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET || '',
+}
+
+const twitterizeRequest = twitterize(oAuthOptions)
+
+const requestOptions: RequestOptions = {
+  requestMethod: 'GET',
+  endpoint: '/search/tweets.json',
+  queryParams: { q: 'twitter bot' },
+}
+
+twitterizeRequest<Tweets>(requestOptions)
+  .then((data) => console.log(data.search_metadata))
+  .catch((error) => console.log(error))
